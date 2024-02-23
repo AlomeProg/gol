3feaf9e9f54638efed78b662fa4d220c1b4604e2
AlomeProg <alomeprog@gmail.com>
2024-02-23 13:15:51 +0300

    Добавлена логика для ограниченной незамкнутой карты

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ using Map = std::array<std::array<bool, size_map>, size_map>;
 Map mapA;  
 Map mapB;
 Map *current_map;
-Map *previous_map;
+Map *next_map;
 
 void InitMap()
 {
@@ -29,7 +29,7 @@ void InitMap()
   }
 
   current_map   = &mapA;
-  previous_map  = &mapB;
+  next_map  = &mapB;
 }
 
 void DrawMap()
@@ -46,12 +46,58 @@ void DrawMap()
   }
 }
 
+int CheckNeighbors(std::size_t x, std::size_t y)
+{
+  int neighbors = 0;
+
+  for (int iy = -1; iy <= 1; iy++)
+  {
+    for (int ix = -1; ix <= 1; ix++)
+    {
+      if (iy == 0 && ix == 0) continue;
+      int new_y = y + iy;
+      int new_x = x + ix;
+      
+      if (new_y < 0 || new_y >= size_map) continue;
+      if (new_x < 0 || new_x >= size_map) continue;
+
+      if (current_map->at(new_y).at(new_x) == true) 
+        neighbors++; 
+    }
+  }
+
+  return neighbors;
+}
+
+void TickMap()
+{
+  for(std::size_t y = 0; y < size_map; y++)
+  {
+    for(std::size_t x = 0; x < size_map; x++)
+    {
+      int neighbors = CheckNeighbors(x, y);
+
+      if (neighbors == 3 && current_map->at(y).at(x) == false)
+        next_map->at(y).at(x) = true;
+
+      if (neighbors == 2 || neighbors == 3) 
+        next_map->at(y).at(x) = true;
+
+      if (neighbors < 2 || neighbors > 3)
+        next_map->at(y).at(x) = false;
+    }
+  }
+  std::swap(current_map, next_map);
+}
+
 int main()
 {
   InitWindow(800, 600, "gol");
   InitMap();
   while (!WindowShouldClose())
   {
+    TickMap();
+
     BeginDrawing();
     ClearBackground(BLACK);
     DrawMap();
